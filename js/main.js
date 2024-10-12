283af152813a068c4e4ccf0367efcf14f16f33e5
mfoste70 <mfoste70@stumail.jccc.edu>
2024-10-12 14:07:35 -0500

    Update proj for Lab 4, interactive images

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 $("button:first").text("CLICK ME for science")
 $(".panel:first > p").text("Thank you for clicking me.")
+
+$(".mod").hover(function(){
+  $(this).css("width", "100%", "transition", "10s");
+  }, function() {
+    $(this).animate({
+      width: "50%",
+    })
+  });
